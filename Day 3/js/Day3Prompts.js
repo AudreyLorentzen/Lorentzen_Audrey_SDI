80d60cb4bb840ed3fff973e1498272557feaf2b8
AudreyLorentzen <aclorentzen@fullsail.edu>
2015-03-07 16:29:25 -0500

    How to prompt correctly

---
+++ Day 3/js/Day3Prompts.js
@@ -0,0 +1,37 @@
+/*
+Audrey Lorentzen
+3-7-15
+Day 3 Prompts
+Section 00
+ */
+// FIX THIS
+
+//alert("Just testing.");
+
+//Ask the user for input
+//prompt("Text that the user will see.");
+
+var userInput = prompt("Enter year of birth.");
+console.log(userInput);
+
+//Calculate the area of a rectangle
+//length * width
+
+//Prompt the user for the width and height
+//Explain to them whats going on.
+var height = prompt("Let's calculate the area of a rectangle!\nPlease enter the height.");
+
+var width = prompt("Please enter the width of the rectangle:");
+
+//Calculate the area of the rectangle
+var area = width*height;
+
+//tell the user the answer
+console.log("The area of the rectangle with a height of "+height+" and a width of "+width+" is "+area+".");
+
+var results = "The results of the area are "+area+".";
+
+console.log(results);
+
+
+
