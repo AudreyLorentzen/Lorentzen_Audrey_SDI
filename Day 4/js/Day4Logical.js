9adc015847b9f4af56cffecc21e5ea9e89178e36
AudreyLorentzen <aclorentzen@fullsail.edu>
2015-03-10 16:37:55 -0400

    Day 4 Logical
    Learned about logical operators and stuff

---
+++ Day 4/js/Day4Logical.js
@@ -0,0 +1,59 @@
+
+ /*
+Audrey Lorentzen
+Section 00
+3-7-15
+Day 4 Logical
+*/
+
+
+ //alert("Just testing.");
+
+ //Basic conditional, if we have enough to buy a car
+
+ //Create a few variables
+ var budget = 15000;
+ var carPrice = 23000;
+ var payCheck = 16000;
+
+ if (budget>=carPrice){
+     console.log("You can afford your car!");
+
+ } else {
+     console.log("You are broke! Take the bus!");
+ }
+
+
+ //We can buy the car IF our budget is greater than or equal to
+
+ if(budget >= carPrice || payCheck >1500){
+     
+ };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //You can buy a car of if the budget is greater than or equal to the car price AND your paycheck is over 1500.
+
+ if(budget >= carPrice && payCheck > 15000){
+     console.log("You can buy the car!");
+ } else{
+     console.log("Pick a cheaper car or find a better job.");
+ }
+
