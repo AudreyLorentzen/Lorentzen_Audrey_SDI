1d04d0cf72ffdad8d5aae4a2dfd523034ebfdd57
AudreyLorentzen <aclorentzen@fullsail.edu>
2015-03-17 13:53:46 -0400

    Bottles of Beer
    Condtionals and for loop

--- Day 7/js/Day7Beer.js
+++ Day 7/js/Day7Beer.js
@@ -8,4 +8,25 @@ Day 7 Beer
 
  //alert("Still Testing.");
 
+ //Code the song 99 Bottles of beer on the wall.
+
+ //Basic structure of for loop
+ // for(var i = 0; condition to test; increment of change){}
+
+ for(var i =99; i>0; i--)
+ {
+     if(i===1)
+     {
+         console.log("1 bottle of beer on the wall. 1 bottle of beer. You take one down and pass it around. 0 bottles of beer on the wall.");
+     }
+     else
+     {
+         console.log(i+" bottles of beer on the wall. "+i+" bottles of beer. You take one down and pass it around. "+(i-1)+" bottles of beer on the wall.");
+
+     }
+
+
+
+ }
+
 
