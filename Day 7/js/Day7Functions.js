96729fb35df08365687432bdcba34105fb0e2663
AudreyLorentzen <aclorentzen@fullsail.edu>
2015-03-17 15:25:31 -0400

    Function call

--- Day 7/js/Day7Functions.js
+++ Day 7/js/Day7Functions.js
@@ -6,6 +6,61 @@ Section 00
 Day 7 Fucntions
 */
 
+
+ // function call before it is defined in our code
+ printHello();
+
  //alert("Still Testing.");
+
+ // Basic structure of a function
+ // function functionName(){ code to run }
+
+ // Create a function that will concole log out hello
+ function printHello()
+ {
+     console.log("Hello and Howdy!")
+ }
+
+ // Create a function that prints out more text
+
+ function printMore()
+ {
+     console.log("Print more text!");
+ }
+
+ // function call the printHello function
+ // functionName();
+ printHello();
+ printMore();
+ printHello();
+
+ // function call the printMore
+ printMore();
+
+ // Create a function that calculates the area of a rectangle
+
+ function calcArea()
+ {
+     // Create variable for width, height, and area
+
+     var width = 20;
+     var height = 30;
+     var area = width * height;
+
+     // Print out the area
+
+     console.log("The area is "+area+".");
+
+
+ }
+
+ // Function call the calcArea
+
+ calcArea();
+
+
+
+
+
 
 
