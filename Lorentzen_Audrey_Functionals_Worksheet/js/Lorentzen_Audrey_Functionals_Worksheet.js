444352b7e031bf630df9ddf39047507133f1df7a
AudreyLorentzen <aclorentzen@fullsail.edu>
2015-03-17 18:45:21 -0400

    Bzzzzzz
    Depending on the victims weight it will take X bee stings to kill
    them..
    FINISHED

--- Lorentzen_Audrey_Functionals_Worksheet/js/Lorentzen_Audrey_Functionals_Worksheet.js
+++ Lorentzen_Audrey_Functionals_Worksheet/js/Lorentzen_Audrey_Functionals_Worksheet.js
@@ -5,14 +5,15 @@ Section 00
 3-7-15
 Lorentzen Audrey Functionals Worksheet
 */
-
+var radius = 10;
  //alert("Still Testing.");
+
 
  //Create a function to calculate the circumference of a circle
 
  function cirArea(r)
  {
-     //Calcculate the area using Pi * r * r
+     //Calculate the area using Pi * r * r
      var d = 2*r;
      var c = Math.PI * d;
 
@@ -22,7 +23,28 @@ Lorentzen Audrey Functionals Worksheet
  }
  //function call this circleArea
  //Create a variable to CATCH the returned area
- var circleArea = cirArea(10);
+ var circleArea = cirArea(radius);
 
  //Console the results
  console.log("The circumference of the circle is "+circleArea+".");
+
+
+ //Create a function to calculate how many bee stings are needed to kill an animal
+ var weight = 400;
+
+ function viciousBees(victimsWeight)
+ {
+     //Calculate how many bee stings using bee stings * victims weight
+     var beestings = victimsWeight * 8.666666667;
+
+     //Return the value
+     return beestings;
+
+
+ }
+
+ ////function call this circleArea
+ //Create a variable to CATCH the returned area
+ var Bzzz = viciousBees(weight);
+
+ console.log("It takes "+Bzzz+" bee stings to kill this animal.");
