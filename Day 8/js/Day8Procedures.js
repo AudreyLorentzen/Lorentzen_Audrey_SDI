962effe541a003711a07d09a47247b279376c525
AudreyLorentzen <aclorentzen@fullsail.edu>
2015-03-24 15:20:40 -0400

    Procedures done

--- Day 8/js/Day8Procedures.js
+++ Day 8/js/Day8Procedures.js
@@ -8,3 +8,29 @@ Day 8 Procedures
 
  //alert("Still Testing.");
 
+ // This is a basic function
+ function calcAreaF(w,h)
+ {
+     var area = w*h;
+     return area;
+ }
+
+//Function Call
+ var area = calcAreaF(6,8);
+ console.log("The area of the rectangle is "+area+".");
+
+ //This is a procedure
+ //A list of steps to do
+
+ function calcAreaP(w,h)
+ {
+     var area = w*h;
+     //We do NOT return this value
+     console.log("Inside the procedure th area is "+area+".");
+ }
+
+ //Function call for the procedure?
+ //Just the function name No variable to catch it
+ calcAreaP(12,16);
+
+
